10a3dd04335206248424e861a751dbe32ca6a09d
cristhian <cral2204@gmail.com>
2023-02-24 09:02:38 -0500

    3. addEventListener

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 console.log('Hello, world')
 
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const id_calculo_1 = document.querySelector('#id_calculo-1');
 const id_calculo_2 = document.querySelector('#id_calculo-2');
-const bnt_calcular = document.querySelector('#btn_calcalar');
+const bnt_calcular = document.querySelector('#bnt_calcular');
 const result = document.querySelector('#result');
 
-function btnOnClick(){
+// Escuchar eventos con addEventListener
+form.addEventListener('submit', btnClick);
+
+function btnClick(event){
     const suma =  id_calculo_1.value + id_calculo_2.value;
-    result.innerHTML = "La suma es: "+suma;
+    result.innerText = "La suma es: "+suma;
 }
 
 
