3d42882e40e75e2462891d5e77e46d2171920fce
cristhian <cral2204@gmail.com>
2023-02-24 08:17:09 -0500

    2. onclick

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 console.log('Hello, world')
 
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const clase_parrafo = document.querySelector('.clase_parrafo');
-const id_parrafo = document.querySelector('#id_parrafo');
-const input = document.querySelector('input');
+const id_calculo_1 = document.querySelector('#id_calculo-1');
+const id_calculo_2 = document.querySelector('#id_calculo-2');
+const bnt_calcular = document.querySelector('#btn_calcalar');
+const result = document.querySelector('#result');
 
-console.log(input.value)
+function btnOnClick(){
+    const suma =  id_calculo_1.value + id_calculo_2.value;
+    result.innerHTML = "La suma es: "+suma;
+}
 
-console.log({
-    h1,
-    p,
-    clase_parrafo,
-    id_parrafo,
-    input
-});
 
-// Cambiar elementos del párrafo
-h1.innerHTML = "holaa";
-// Agregar elementos al html
-const img = document.createElement('img');
-img.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png')
-console.log(img)
-id_parrafo.append(img)
